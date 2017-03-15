9684363e4f9e374b0db9d23744d65398244f59b4
lionel lints <lionel.lints@gmail.com>
2017-03-14 22:00:03 -0700

    add thunk and promise async middleware ⏰

--- src/js/store.js
+++ src/js/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 
 import logger from 'redux-logger';
-// import thunk from 'redux-thunk';
-// import promise from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 
 import reducer from './reducers';
 
-const middleware = applyMiddleware(logger()/*, thunk, promise()*/);
+const middleware = applyMiddleware(logger(), thunk, promise());
 export default createStore(reducer, {}, middleware);
